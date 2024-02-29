5bcc2343d169ff6c8538cee2620a0aded4656dc4
Koeboldt <142840132+Koeboldt@users.noreply.github.com>
2024-02-28 18:51:58 -0600

    impletmented get and put with idb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putting stuff in the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const storage = tx.objectStore('jate');
+  const request = storage.put({ id: id, content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getting stuff from database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const storage= tx.objectStore('jate');
+  const request = storage.getAll();
+  const result = await request;
+  return result;
+};
 
 initdb();
