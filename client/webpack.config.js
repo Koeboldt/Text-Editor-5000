a67cfb8cf1d1652df7a4755978761d383380b59f
Koeboldt <142840132+Koeboldt@users.noreply.github.com>
2024-02-28 18:06:54 -0600

    service worker is now setup, and now will cache

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,13 +16,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor 5000'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor for you and me',
+        background_color: '#000000',
+        theme_color: '#000000',
+        start_url: './',
+        publicPath: './'
+      })
     ],
 
     module: {
-      rules: [
-        
-      ],
+      rules: [{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\,m?js$/,
+      exclude: /node_modules/,
+      use{
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        }
+      }
+    }
+  ],
     },
   };
 };
